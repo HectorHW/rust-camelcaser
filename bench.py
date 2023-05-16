045a2c126239ac9ba5aa34325bbdbd26779f1944
Редькин Владимир Сергеевич <v.s.redkin@tinkoff.ru>
2023-05-16 16:40:38 +0300

    annotate output

--- bench.py
+++ bench.py
@@ -10,8 +10,8 @@ big_string = "data" + "_data" * 100
 python_speed = timeit.timeit("to_camel_py(big_string)", globals=globals())
 rust_speed = timeit.timeit("capitalize_rust(big_string)", globals=globals())
 
-print(python_speed)
-print(rust_speed)
+print("python:", python_speed)
+print("rust:", rust_speed)
 
 speedup = (python_speed - rust_speed) / python_speed
 
